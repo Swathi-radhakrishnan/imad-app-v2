7409b94969a3da5211e7c22c4715e45cd4df0e49
Swathi-radhakrishnan <15tucs244@skct.edu.in>
2017-02-18 19:58:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 var image=document.getElementById('mad');
 var marginLeft =0;
 function moveRight() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     image.style.marginLeft=marginLeft+'px';
 }
 image.onclick =  function() {
-    var interval =setInterval(moveRight,100);
+    var interval =setInterval(moveRight,50);
 };
