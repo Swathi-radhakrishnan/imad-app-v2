1c34d7938ed08fc00ade1c962d74d608e5505952
Swathi-radhakrishnan <15tucs244@skct.edu.in>
2017-02-18 19:30:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ res.send('Article three requested and will be served here');
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.jss', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.jss'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
